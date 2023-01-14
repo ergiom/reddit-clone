f9f21e033ba492b8fddf66060f2b7a64b67f9723
ergiom <ergiom1@gmail.com>
2023-01-14 20:03:30 +0100

    added user controller and service

---
+++ src/main/java/com/example/redditclone/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.example.redditclone.controller;
+
+import com.example.redditclone.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+}
---
+++ src/main/java/com/example/redditclone/service/UserService.java
@@ -0,0 +1,7 @@
+package com.example.redditclone.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
