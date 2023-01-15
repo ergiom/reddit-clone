a04db7f0d39b3b41be780ed4d14c03552d47668b
ergiom <ergiom1@gmail.com>
2023-01-15 07:54:23 +0100

    added table specification to entities

--- src/main/java/com/example/redditclone/entity/Post.java
+++ src/main/java/com/example/redditclone/entity/Post.java
@@ -8,9 +8,11 @@ import lombok.NoArgsConstructor;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "post_tbl")
 @Data
 @Builder
 @NoArgsConstructor
--- src/main/java/com/example/redditclone/entity/User.java
+++ src/main/java/com/example/redditclone/entity/User.java
@@ -6,15 +6,19 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "user_tbl")
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
     @Id
+    @GeneratedValue
     private Long id;
     private String name;
 }
