de4709cc91c2362a167e90c14bc96691ceedbce1
ergiom <ergiom1@gmail.com>
2023-01-15 07:53:22 +0100

    filled schema and data sql scripts

---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO user_tbl(id, name) VALUES (1, 'Tom');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,39 @@
+CREATE TABLE user_tbl (
+    id BIGINT,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
+CREATE TABLE subreddit_tbl (
+    id BIGINT,
+    owner_id BIGINT,
+    name VARCHAR(255),
+    PRIMARY KEY (id),
+    FOREIGN KEY (owner_id) REFERENCES user_tbl(id)
+);
+CREATE TABLE post_tbl (
+    subreddit_id BIGINT,
+    id BIGINT,
+    author_id BIGINT,
+    title VARCHAR(255),
+    content VARCHAR(255),
+    published DATE,
+    last_edited DATE,
+    PRIMARY KEY (subreddit_id, id),
+    FOREIGN KEY (subreddit_id) REFERENCES subreddit_tbl(id),
+    FOREIGN KEY (author_id) REFERENCES user_tbl(id)
+);
+CREATE TABLE comment_tbl (
+    subreddit_id BIGINT,
+    post_id BIGINT,
+    id BIGINT,
+    author_id BIGINT,
+    parent_comment_id BIGINT,
+    content VARCHAR(255),
+    published DATE,
+    last_edited DATE,
+    PRIMARY KEY (subreddit_id, post_id, id),
+    FOREIGN KEY (subreddit_id) REFERENCES subreddit_tbl(id),
+    FOREIGN KEY (subreddit_id, post_id) REFERENCES post_tbl(subreddit_id, id),
+    FOREIGN KEY (author_id) REFERENCES user_tbl(id),
+    FOREIGN KEY (subreddit_id, post_id, parent_comment_id) REFERENCES comment_tbl(subreddit_id, post_id, id)
+);
